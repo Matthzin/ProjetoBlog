e4a9d3ccab6d431b8c4debb4729f0cbf21131503
Matheus Avelino <avelinosantos09@gmail.com>
2024-08-26 21:33:01 -0300

    refatorações em código e estilização

---
+++ home.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const postagens = JSON.parse(localStorage.getItem("postagens")) || [];
+  console.log(postagens);
+  console.log("aqui");
+
+  const artigoDestaque = document.getElementById("artigo-destaque");
+  const artigoRecente = document.getElementById("artigo-recente");
+
+  if (postagens.length > 0) {
+    const postagem = postagens[0];
+    const postagemDiv = document.createElement("div");
+    postagemDiv.className = "post-destaque"
+    postagemDiv.classList.add("postagem");
+    
+    const titulo = document.createElement("h3");
+    const breakline = document.createElement("br");
+    titulo.textContent = postagem.titulo;
+    postagemDiv.appendChild(titulo);
+    postagemDiv.appendChild(breakline);
+    
+    const conteudo = document.createElement("p");
+    conteudo.textContent = postagem.conteudo;
+    postagemDiv.appendChild(conteudo);
+    
+    const data = document.createElement("p");
+    data.classList.add("data");
+    data.textContent = `Publicado em: ${postagem.data}`;
+    postagemDiv.appendChild(data);
+
+    artigoDestaque.appendChild(postagemDiv);
+
+    for (let i = 1; i < postagens.length; i++) {
+      const postagemRecent = postagens[i];
+
+      const postagemRecentDiv = document.createElement("div");
+      postagemRecentDiv.classList.add("postagem-recente");
+
+      const tituloRecent = document.createElement("h3");
+      tituloRecent.textContent = postagemRecent.titulo;
+      postagemRecentDiv.appendChild(tituloRecent);
+
+      const conteudoRecent = document.createElement("p");
+      conteudoRecent.textContent = postagemRecent.conteudo;
+      postagemRecentDiv.appendChild(conteudoRecent);
+
+      const dataRecent = document.createElement("p");
+      dataRecent.classList.add("data");
+      dataRecent.textContent = `Publicado em: ${postagemRecent.data}`;
+      postagemRecentDiv.appendChild(dataRecent);
+
+      artigoRecente.appendChild(postagemRecentDiv);
+    }
+  } else {
+    const mensagem = document.createElement("p");
+    mensagem.textContent = "Não há postagens para exibir.";
+    artigoDestaque.appendChild(mensagem);
+  }
+});
